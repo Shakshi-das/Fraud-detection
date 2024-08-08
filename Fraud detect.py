2a3e12af15f4fdcc25b3440982692d42670a9ef4
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-08-08 23:43:23 +0530

    Update Fraud detect.py
    fitting a PCA transform

--- Fraud detect.py
+++ Fraud detect.py
@@ -1,14 +1,12 @@
-Importing libraries:
 
 #essentials
-import numpy as np
 import pandas as pd
-import datetime
-import random
 
-# Plots
-import seaborn as sns
-import matplotlib.pyplot as plt
+df = pd.read_csv('Data/creditcard.csv')
+df.head()
+
+df.info()
+df['Class'].value_counts()
 
 # Separate the samples by class
 legit = df[df['Class'] == 0]
@@ -17,3 +15,9 @@ fraud = df[df['Class'] == 1]
 # Drop the "Time" and "Class" columns
 legit = legit.drop(['Time', 'Class'], axis=1)
 fraud = fraud.drop(['Time', 'Class'], axis=1)
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=26, random_state=0)
+legit_pca = pd.DataFrame(pca.fit_transform(legit), index=legit.index)
+fraud_pca = pd.DataFrame(pca.transform(fraud), index=fraud.index)
