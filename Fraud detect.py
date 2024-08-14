4b3d21eb96d44c8678c6dc50fa0195bcf932cf29
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-08-15 03:04:42 +0530

    Update Fraud detect.py
    plotting the loss

--- Fraud detect.py
+++ Fraud detect.py
@@ -25,11 +25,24 @@ fraud_pca = pd.DataFrame(pca.transform(fraud), index=fraud.index)
 legit_restored = pd.DataFrame(pca.inverse_transform(legit_pca), index=legit_pca.index)
 fraud_restored = pd.DataFrame(pca.inverse_transform(fraud_pca), index=fraud_pca.index)
 
+#function for measuring loss due to PCA.
+import numpy as np
+
+def get_anomaly_scores(df_original, df_restored):
+    loss = np.sum((np.array(df_original) - np.array(df_restored)) ** 2, axis=1)
+    loss = pd.Series(data=loss, index=df_original.index)
+    return loss
+
+legit_scores = get_anomaly_scores(legit, legit_restored)
+fraud_scores = get_anomaly_scores(fraud, fraud_restored)
+
 #plot the loss for the legitimate transactions
 import matplotlib.pyplot as plt
 %matplotlib inline
 import seaborn as sns
 sns.set()
+
+legit_scores.plot(figsize = (12, 6))
 
 #Plot the loss for the fraudulent transactions.
 fraud_scores.plot(figsize = (12, 6))
